699433cb2a10c3dc426650c2de39ce1c75a6dbe0
Danilo MS <124013111+LICObr@users.noreply.github.com>
2024-10-19 09:11:02 -0300

    Create EX2.1.c

---
+++ Exercicios Strings/EX2.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[50], str2[50];
+
+    // Ler as duas strings
+    
+    printf("Digite o primeiro nome: ");
+    scanf("%s", str1);
+
+    printf("Digite o segundo nome: ");
+    scanf("%s", str2);
+
+    // Comparar as strings
+    
+    if(strcmp(str1, str2) == 0) {
+        printf("As strings sao iguais.\n");
+    } else {
+        printf("As strings sao diferentes.\n");
+    }
+
+    return 0;
+}
